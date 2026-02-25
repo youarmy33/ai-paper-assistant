c1e8cc82f9cedf4f5e4657055e0b54ac0956af3c
youarmy33 <wangzhenchao33@gmail.com>
2026-02-25 17:13:23 +0800

    Add built-in paper templates

---
+++ src/templates/paper_templates.py
@@ -0,0 +1,67 @@
+# Paper Templates
+
+# Template for Research Paper
+research_paper_template = '''
+Title: {title}
+
+Abstract:
+{abstract}
+
+Introduction:
+{introduction}
+
+Literature Review:
+{literature_review}
+
+Methods:
+{methods}
+
+Results:
+{results}
+
+Discussion:
+{discussion}
+
+Conclusion:
+{conclusion}
+'''  
+
+# Template for Review Paper
+review_paper_template = '''
+Title: {title}
+
+Abstract:
+{abstract}
+
+Objective:
+{objective}
+
+Methodology:
+{methodology}
+
+Findings:
+{findings}
+
+Implications:
+{implications}
+
+Conclusion:
+{conclusion}
+'''  
+
+# Template for Conference Paper
+conference_paper_template = '''
+Title: {title}
+
+Abstract:
+{abstract}
+
+Introduction:
+{introduction}
+
+Methods:
+{methods}
+
+Conclusions:
+{conclusions}
+'''  
